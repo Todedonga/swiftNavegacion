0c2375ae96fedbbc4bb5c237c6b864c350ad89e1
Todedonga <guillermo.flores@iest.edu.mx>
2025-09-29 17:53:07 -0600

    Initial Commit

---
+++ NavegacionTests/NavegacionTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavegacionTests.swift
+//  NavegacionTests
+//
+//  Created by win603 on 29/09/25.
+//
+
+import Testing
+@testable import Navegacion
+
+struct NavegacionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
